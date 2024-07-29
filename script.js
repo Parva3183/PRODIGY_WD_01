18270816d820d400270e4f3437823f08b7d0f4cf
Parva3183 <122863774+Parva3183@users.noreply.github.com>
2024-07-29 14:34:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    mobileMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    const navItems = document.querySelectorAll('.nav-links a');
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });
+});
